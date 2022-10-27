a2f0c2552d6e1ea87c0c652f398d057252334f1a
karanjavali <knjavali@asu.edu>
2022-10-27 11:40:43 -0700

    first function skeleton

---
+++ src/main/java/urinals.java
@@ -0,0 +1,17 @@
+/*
+ * Author - Karan Navin Javali
+ * ASURITE - knjavali
+ * ASU ID - 1225469752
+ */
+
+public class urinals {
+    Boolean goodString( String str ) { // checks to see if valid string
+        System.out.println("Not yet implemented");
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
