c38a8f20081f77c1691b86b0620afe600c6aa929
karanjavali <knjavali@asu.edu>
2022-10-27 20:43:04 -0700

    file generation started, will proceed to write output

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,13 +6,56 @@ import java.util.Scanner;
  * @author Karan Navin Javali
  */
 public class Main {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) throws IOException {
         urinals obj = new urinals();
-        String path = new File("src/main/java/urinal.dat").getAbsolutePath();
-        File file = new File(path);
-        Scanner sc = new Scanner(file);
-        while(sc.hasNextLine()){
-            System.out.println(obj.numberOfUrinals(sc.nextLine()));
+        Scanner inputSc = new Scanner(System.in);
+
+        // Choose input option
+        System.out.println("Choose the input method (Enter 1 or 2)");
+
+        System.out.println("1. Inputs from file (name the file urinal.dat)");
+        System.out.println("2. Inputs from keyboard (one string input will be accepted)");
+
+        int input = inputSc.nextInt();
+        switch (input) {
+
+            case 1:
+                // input taken from urinal.dat file and printed to rule.txt/rule(i).txt where i is file number(1,2,..) if previous numbers exist
+                String path = new File("src/main/java/urinal.dat").getAbsolutePath();
+                File file = new File(path);
+                Scanner sc = new Scanner(file);
+                while(sc.hasNextLine()){
+                    System.out.println(obj.numberOfUrinals(sc.nextLine()));
+                }
+                String completePath = "src/main/java/";
+                File myObj = new File(completePath+"rule.txt");
+                if (myObj.createNewFile()) {
+                    System.out.println("File created: " + myObj.getName());
+                } else {
+                    // create rule1.txt or rule2.txt .....
+                    String fileName = "rule";
+                    int i = 1;
+                    while(true) {
+                        File newFile = new File(completePath+fileName+ i +".txt");
+                        if (newFile.createNewFile()) {
+                            break;
+                        }
+                        else {
+                            i++;
+                        }
+                    }
+                }
+                break;
+
+            case 2:
+                // Enter input and print output
+                System.out.println("Please enter the input string");
+                inputSc.nextLine();
+                String str = inputSc.nextLine();
+                System.out.println(obj.numberOfUrinals(str));
+                break;
+            default:
+                System.out.println("Invalid input");
         }
 
     }
